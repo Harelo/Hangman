3eab9a4ab5fda225bba373e4bb886f6105c05ec1
Hareloo <harel123@windowslive.com>
2019-08-30 05:00:34 +0300

    Initial Commit

---
+++ HangmanGame.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class hmGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.word = new System.Windows.Forms.TextBox();
+            this.chooseLetter = new System.Windows.Forms.Button();
+            this.guessesLeft = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.letter = new System.Windows.Forms.TextBox();
+            this.newWord = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // word
+            // 
+            this.word.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.word.Location = new System.Drawing.Point(42, 12);
+            this.word.Multiline = true;
+            this.word.Name = "word";
+            this.word.ReadOnly = true;
+            this.word.Size = new System.Drawing.Size(138, 44);
+            this.word.TabIndex = 0;
+            this.word.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // chooseLetter
+            // 
+            this.chooseLetter.Enabled = false;
+            this.chooseLetter.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.chooseLetter.Location = new System.Drawing.Point(42, 75);
+            this.chooseLetter.Name = "chooseLetter";
+            this.chooseLetter.Size = new System.Drawing.Size(138, 62);
+            this.chooseLetter.TabIndex = 1;
+            this.chooseLetter.Text = "Click to guess the letter below";
+            this.chooseLetter.UseVisualStyleBackColor = true;
+            this.chooseLetter.Click += new System.EventHandler(this.chooseLetter_Click);
+            // 
+            // guessesLeft
+            // 
+            this.guessesLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.guessesLeft.Location = new System.Drawing.Point(128, 206);
+            this.guessesLeft.Multiline = true;
+            this.guessesLeft.Name = "guessesLeft";
+            this.guessesLeft.ReadOnly = true;
+            this.guessesLeft.Size = new System.Drawing.Size(41, 21);
+            this.guessesLeft.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Guesses left:";
+            // 
+            // letter
+            // 
+            this.letter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.letter.Location = new System.Drawing.Point(101, 143);
+            this.letter.Multiline = true;
+            this.letter.Name = "letter";
+            this.letter.Size = new System.Drawing.Size(30, 28);
+            this.letter.TabIndex = 4;
+            this.letter.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.letter.Visible = false;
+            this.letter.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.letter_KeyPress);
+            // 
+            // newWord
+            // 
+            this.newWord.Location = new System.Drawing.Point(57, 273);
+            this.newWord.Name = "newWord";
+            this.newWord.Size = new System.Drawing.Size(110, 23);
+            this.newWord.TabIndex = 5;
+            this.newWord.Text = "Generate new word";
+            this.newWord.UseVisualStyleBackColor = true;
+            this.newWord.Click += new System.EventHandler(this.newWord_Click);
+            // 
+            // hmGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(221, 335);
+            this.Controls.Add(this.newWord);
+            this.Controls.Add(this.letter);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guessesLeft);
+            this.Controls.Add(this.chooseLetter);
+            this.Controls.Add(this.word);
+            this.Name = "hmGame";
+            this.Text = "Hangman";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox word;
+        private System.Windows.Forms.Button chooseLetter;
+        private System.Windows.Forms.TextBox guessesLeft;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox letter;
+        private System.Windows.Forms.Button newWord;
+    }
+}
+
---
+++ HangmanGame.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class hmGame : Form
+    {
+        static List<char> letters = new List<char>();
+        static int pos = 0;
+        static int wordChosen;
+        static int guessLeft;
+        static string theWordChosen;
+        static List<string> Words = new List<string>();
+        public hmGame()
+        {
+            loadWords();
+            InitializeComponent();
+        }
+        private void loadWords()
+        {
+            string path = System.IO.Directory.GetCurrentDirectory();
+            string wordsFileDirectory = path+"\\words.txt";
+            if (System.IO.File.Exists(wordsFileDirectory) && new System.IO.FileInfo(wordsFileDirectory).Length != 0)
+            {   
+                System.IO.StreamReader wordsLoader = new System.IO.StreamReader(wordsFileDirectory);
+                int i = 0;
+                while (wordsLoader.Peek() != -1)
+                {
+                        Words.Insert(i, wordsLoader.ReadLine());
+                        i++;    
+                }
+                wordsLoader.Close();
+            }
+            else if (!System.IO.File.Exists(wordsFileDirectory))
+            {
+                MessageBox.Show("Words file was not found! We'll create one for you, already loaded with 5 words!");
+                createWordsFile();
+            }
+            else if (new System.IO.FileInfo(wordsFileDirectory).Length == 0)
+            {
+                MessageBox.Show("Words file was found but is empty, we'll put some words for you!");
+                createWordsFile();
+            }
+        }
+        private void createWordsFile()
+        {
+            string path = System.IO.Directory.GetCurrentDirectory();
+            string wordsFileDirectory = path + "\\words.txt";
+            System.IO.StreamWriter wordsFileWriter = new System.IO.StreamWriter(wordsFileDirectory);
+            wordsFileWriter.WriteLine("watermelon");
+            wordsFileWriter.WriteLine("car");
+            wordsFileWriter.WriteLine("mouse");
+            wordsFileWriter.WriteLine("computer");
+            wordsFileWriter.WriteLine("school");
+            wordsFileWriter.Close();
+            loadWords();
+        }
+        private void chooseLetter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                char ltr = char.Parse(letter.Text);
+                int letterPos = Words[wordChosen].IndexOf(ltr.ToString().ToLower());
+                if (!letters.Contains(ltr))
+                {
+                    if (letterPos != -1)
+                    {
+                        while (letterPos != -1)
+                        {
+                            word.Text = word.Text.Remove(letterPos, 1);
+                            word.Text = word.Text.Insert(letterPos, ltr.ToString().ToLower());
+                            Words[wordChosen] = Words[wordChosen].Remove(letterPos, 1);
+                            Words[wordChosen] = Words[wordChosen].Insert(letterPos, "*");
+                            letterPos = Words[wordChosen].IndexOf(ltr.ToString().ToLower());
+                            if (!word.Text.Contains('*'))
+                                gameOver(true);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Letter wasn't found in the word!");
+                        guessLeft -= 1;
+                        guessesLeft.Text = guessLeft.ToString();
+                        if (guessLeft == 0)
+                            gameOver(false);
+                    }
+                    if (guessLeft != -1)
+                    {
+                        letter.Text = null;
+                        letters.Insert(pos, ltr);
+                        pos++;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Letter was already guessed!");
+                    letter.Text = null;
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid letter, please try again!");
+                letter.Text = null;
+            }
+        }
+
+        private void newWord_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int rnd = r.Next(0, Words.Count);
+            string savedWord = Words[rnd];
+            for (int i=0; i < Words[rnd].Length; i++)
+            {
+                Words[rnd] = Words[rnd].Remove(i, 1).Insert(i, "*");
+            }
+            word.Text = Words[rnd];
+            wordChosen = rnd;
+            Words[rnd] = savedWord;
+            chooseLetter.Enabled = true;
+            letter.Visible = true;
+            guessLeft = 6;
+            guessesLeft.Text = guessLeft.ToString();
+            theWordChosen = Words[wordChosen];
+            letters.Clear();
+        }
+        private void gameOver(bool wordGuessed)
+        {
+            if (!wordGuessed)
+                MessageBox.Show("Word was not guessed, the man was hanged D:!");
+            else if (wordGuessed)
+                MessageBox.Show("Word was guessed correctly, congratulations!");
+            chooseLetter.Enabled = false;
+            letter.Visible = false;
+            letter.Text = null;
+            letters.Clear();
+            guessLeft = -1;
+            guessesLeft.Text = null;
+            word.Text = null;
+            Words[wordChosen] = theWordChosen;
+            theWordChosen = null;
+            pos = 0;
+        }
+
+        private void letter_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !(char.IsLetter(e.KeyChar) && letter.Text.Length < 1 || e.KeyChar == (char)Keys.Back);
+        }
+    }
+}
